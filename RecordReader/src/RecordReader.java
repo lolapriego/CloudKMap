fc88cfa82ae646b8f1cce1f3325e31f6c213752e
stroll <stroll@stroll-VirtualBox.(none)>
2013-10-21 09:47:18 -0500

    Add RecordReader

---
+++ RecordReader/src/RecordReader.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+
+public class RecordReader {
+	
+	public static InputStream LoadSplit(String BucketName, String Key) throws IOException {
+	/*
+	 * Setup s3
+	 */
+        AmazonS3 s3 = new AmazonS3Client(new ClasspathPropertiesFileCredentialsProvider());
+		Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+		s3.setRegion(usWest2);
+		
+	/*
+         * Download an object
+         */
+        System.out.println("Downloading an object");
+        S3Object object = s3.getObject(new GetObjectRequest(BucketName, Key));
+        System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
+        //displayTextInputStream(object.getObjectContent());
+		
+		return object.getObjectContent();
+	}
+	
+	/**
+     * Displays the contents of the specified input stream as text.
+     *
+     * @param input
+     *            The input stream to display as text.
+     *
+     * @throws IOException
+     */
+    private static void displayTextInputStream(InputStream input) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) break;
+
+            System.out.println("    " + line);
+        }
+        System.out.println();
+    }
+}
